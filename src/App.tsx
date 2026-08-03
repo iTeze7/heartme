6c9528c58a4ead204c2b6b90f3dabad986e6b6c6
Higor Endrik <higoroliveira349@gmail.com>
2026-08-03 11:06:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
                   className="pt-8 flex flex-col items-start gap-6"
                 >
                   <p className="text-white/40 italic">
-                    {">"} Mas tem uma coisinha aqui que me lembrou você.
+                    {">"} Mas fiz algo de coração pra você.
                   </p>
                   
                   <button
